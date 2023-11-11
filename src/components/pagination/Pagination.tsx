bba57262060645a6a82882017a4676a1b7c4726a
Duongduy03 <110532713+Duongduy03@users.noreply.github.com>
2023-11-11 15:57:29 +0700

    update

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -38,7 +38,7 @@ const Pagination = ({totalPages, currentPage, onPageChange}:Pagination) => {
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
     
-    <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+    <div className=" sm:flex sm:flex-1 sm:items-center sm:justify-between">
       <div>
         <p className="text-sm text-gray-700">
           Showing <span className="font-medium">1</span> to <span className="font-medium">10</span> of{' '}
