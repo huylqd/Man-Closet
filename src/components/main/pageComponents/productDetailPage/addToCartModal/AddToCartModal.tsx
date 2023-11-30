0f17c50eb412f795f293ffd9974aa23d5986a589
YuongEon <boyjackgamer@gmail.com>
2023-11-30 16:52:17 +0700

    update detail product layout and add to cart func

---
+++ src/components/main/pageComponents/productDetailPage/addToCartModal/AddToCartModal.tsx
@@ -0,0 +1,390 @@
+import { useCurrency } from "@/hooks";
+import { IProduct, Variant } from "@/interfaces/product";
+import { cn } from "@/lib/utils";
+import { addProductToCart } from "@/redux/reducer/cart.reducer";
+import { useAppDispatch } from "@/redux/store";
+import { commonSuccessToast } from "@/utils/notify";
+import { Minus, Plus, X } from "lucide-react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./addToCartModal.scss";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+
+interface AddToCardModalProps {
+  onClose: () => void;
+  isOpen: boolean;
+  product: IProduct;
+}
+
+const AddToCartModal = ({ isOpen, onClose, product }: AddToCardModalProps) => {
+  const router = useRouter();
+  const user = JSON.parse(localStorage.getItem("user") as string);
+
+  const [sizeSelected, setSizeSelected] = useState("");
+  const [colorSelected, setColorSelected] = useState("");
+  const [imageSelected, setImageSelected] = useState("");
+  const [inventoryQuantity, setInventoryQuantity] = useState(0);
+  const [amount, setAmount] = useState("1");
+  const [property, setProperty] = useState("");
+  const [isDisableAdd, setIsDisableAdd] = useState(false);
+
+  const dispatchThunk = useAppDispatch();
+
+  useEffect(() => {
+    if (!isOpen) {
+      return;
+    }
+    document.body.style.marginRight = `${
+      window.innerWidth - document.documentElement.clientWidth
+    }px`;
+    document.body.style.overflow = "hidden";
+    return (): void => {
+      document.body.style.overflow = "unset";
+      document.body.style.marginRight = "0px";
+    };
+  }, [isOpen]);
+
+  useEffect(() => {
+    setImageSelected(product.properties?.[0]?.imageUrl);
+  }, [product]);
+
+  useEffect(() => {
+    getProductQuantityExist();
+    wrapProperty();
+  }, [colorSelected, sizeSelected]);
+
+  useEffect(() => {
+    handleChangeImageSelected();
+  }, [colorSelected]);
+
+  useEffect(() => {
+    toggleDisableAdd();
+    setAmountEqualInventoryWhenHigher();
+  }, [inventoryQuantity, amount]);
+
+  // ===================================
+
+  const wrapProperty = () => {
+    if (sizeSelected === "" || colorSelected === "") {
+      setProperty(sizeSelected || colorSelected);
+    } else if (sizeSelected !== "" && colorSelected !== "") {
+      setProperty([sizeSelected, colorSelected].join(", "));
+    }
+  };
+
+  // ===================================
+
+  const getProductQuantityExist = () => {
+    const indexOfColor = product.properties?.findIndex(
+      (item) => item.color === colorSelected
+    );
+    const indexOfSize = product.properties?.[indexOfColor]?.variants.findIndex(
+      (item) => item.size === sizeSelected
+    );
+    const quantity =
+      product.properties?.[indexOfColor]?.variants?.[indexOfSize]?.quantity;
+    setInventoryQuantity(quantity);
+  };
+
+  // ===================================
+
+  const handleChangeImageSelected = () => {
+    const indexOfColor = product.properties?.findIndex(
+      (item) => item.color === colorSelected
+    );
+    const image = product.properties?.[indexOfColor]?.imageUrl;
+    setImageSelected(image);
+  };
+
+  // ===================================
+
+  const convertVariantArr = (arr: Array<Array<Variant>>) => {
+    if (!arr) {
+      return;
+    }
+    const result = arr.map((item) => {
+      return item[0];
+    });
+
+    return result;
+  };
+
+  const variantArr = convertVariantArr(
+    product.properties?.map((item) => item.variants)
+  );
+
+  // lay unique Color array
+  const colorArray = product.properties
+    ?.map((item) => item.color)
+    .filter(
+      (value, index, self) => self.findIndex((m) => m === value) === index
+    );
+
+  // lay unique Size array
+  const sizeArray = variantArr
+    ?.map((item) => item.size)
+    ?.filter(
+      (value, index, self) => self.findIndex((m) => m === value) === index
+    );
+
+  // ===================================
+
+  const checkAmountInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const valueInput = e.target.value;
+    if (valueInput.match(/^[0-9]*$/)) {
+      if(+valueInput > inventoryQuantity){
+        setAmount(inventoryQuantity.toString())
+      } else {
+        setAmount(valueInput);
+      }
+    }
+  };
+
+  const toggleDisableAdd = () => {
+    if (
+      inventoryQuantity > 0 &&
+      +amount <= inventoryQuantity &&
+      amount !== ""
+    ) {
+      setIsDisableAdd(false);
+    } else {
+      setIsDisableAdd(true);
+    }
+  };
+
+  const setAmountEqualInventoryWhenHigher = () => {
+    if(+amount > inventoryQuantity && inventoryQuantity > 0 && amount !== ""){
+      setAmount(inventoryQuantity.toString())
+    }
+  }
+
+  const checkAmountInputBlur = () => {
+    if (amount.match(/^0+/) || amount === "") {
+      setAmount("1");
+    }
+  };
+
+  const incrementAmount = () => {
+    if (+amount < inventoryQuantity) {
+      setAmount((+amount + 1).toString());
+    }
+  };
+
+  const decrementAmount = () => {
+    if (+amount > 1) {
+      setAmount((+amount - 1).toString());
+    } else {
+      setAmount("1");
+    }
+  };
+
+  // ===================================
+
+  const addToCart = () => {
+    const userId = user._id;
+    if (!userId) {
+      router.push("/auth");
+    }
+
+    let validData = false;
+    if (
+      inventoryQuantity > 0 &&
+      +amount > 0 &&
+      !isNaN(+amount) &&
+      inventoryQuantity >= +amount
+    ) {
+      validData = true;
+    }
+
+    const data = {
+      user_id: userId,
+      product: {
+        _id: product._id,
+        name: product.productName,
+        quantity: +amount,
+        imageUrl: imageSelected,
+        color: colorSelected,
+        size: sizeSelected,
+        price: product.price,
+      },
+    };
+
+    if (validData) {
+      dispatchThunk(addProductToCart(data));
+      onClose();
+      commonSuccessToast("Thêm sản phẩm thành cồng");
+    } else {
+      toast.error("Vui lòng xem lại dữ liệu đã lựa chọn");
+      console.log("no");
+    }
+  };
+
+  return (
+    <>
+      <div
+        onClick={() => onClose()}
+        className={cn(
+          "blur_layer inset-0 fixed z-30",
+          isOpen ? "block" : "hidden"
+        )}
+      ></div>
+
+      <div
+        className={cn(
+          "atc_modal fixed z-50 md:top-0 bottom-0 right-0 bg-white rounded-t-lg md:rounded-l w-full h-[80%] md:h-screen md:w-[46%] lg:w-[360px] p-3 md:p-5  block",
+          isOpen ? "open" : "close"
+        )}
+      >
+        <div className="flex flex-col w-full h-full">
+          <div className="flex justify-between items-start">
+            <div className="flex justify-between items-start gap-2 md:gap-4">
+              <div className="relative w-[60px] h-[60px] md:w-[100px] md:h-[100px] rounded overflow-hidden">
+                <Image
+                  src={imageSelected}
+                  loading="lazy"
+                  alt="product-image"
+                  fill
+                  style={{ objectFit: "cover" }}
+                />
+              </div>
+              <div className="flex flex-col h-[60px] md:h-[100px]">
+                <h4 className="text-xl font-medium">
+                  {useCurrency(product.price)}
+                </h4>
+                <h5 className="py-1 text-base md:text-md font-medium text-gray-800">
+                  {property}
+                </h5>
+                <h5 className="text-md mt-auto">
+                  <span className="text-base md:text-md font-medium text-gray-600">
+                    Trong kho:{" "}
+                  </span>
+                  {colorSelected !== "" && sizeSelected !== "" ? (
+                    inventoryQuantity > 0 ? (
+                      <span className="text-base md:text-md font-medium text-gray-800">
+                        {inventoryQuantity}
+                      </span>
+                    ) : (
+                      <span className="text-base md:text-md font-medium text-rose-500">
+                        Hết hàng
+                      </span>
+                    )
+                  ) : (
+                    ""
+                  )}
+                </h5>
+              </div>
+            </div>
+            <div className="p-1">
+              <X onClick={() => onClose()} className="w-5 h-5 cursor-pointer" />
+            </div>
+          </div>
+
+          <div className="py-4 flex-[1]">
+            <div className="py-2 md:py-4">
+              <h4 className="pb-2 text-base md:text-md text-gray-600 font-medium">
+                Kích cỡ
+              </h4>
+              <ul className="flex flex-wrap gap-2 gap-y-2">
+                {sizeArray?.map((size) => (
+                  <li key={uuidv4()}>
+                    <button
+                      className={cn(
+                        "border border-gray-300 rounded px-3 py-1",
+                        sizeSelected === size
+                          ? "text-[--secondary-color] border-[--secondary-color]"
+                          : "text-gray-800"
+                      )}
+                      onClick={() => setSizeSelected(size)}
+                    >
+                      {size}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="py-2 md:py-4">
+              <h4 className="pb-2 text-base md:text-md text-gray-600 font-medium">
+                Màu sắc
+              </h4>
+              <ul className="flex flex-wrap gap-2 gap-y-2">
+                {colorArray?.map((color) => (
+                  <li key={uuidv4()}>
+                    <button
+                      className={cn(
+                        "border border-gray-300 px-3 rounded py-1",
+                        colorSelected === color
+                          ? "text-[--secondary-color] border-[--secondary-color]"
+                          : "text-gray-800"
+                      )}
+                      onClick={() => setColorSelected(color)}
+                    >
+                      {color}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="py-2 md:py-4">
+              <div className="flex items-center justify-between">
+                <h4 className="text-base md:text-md text-gray-600 font-medium">
+                  Số lượng
+                </h4>
+                <div className="flex items-center gap-x-2">
+                  <button onClick={() => decrementAmount()} className="p-[6px]">
+                    <Minus
+                      className={cn(
+                        "w-5 h-5",
+                        +amount <= 1 || amount === ""
+                          ? "text-gray-400 cursor-not-allowed"
+                          : "text-gray-800 cursor-pointer"
+                      )}
+                    />
+                  </button>
+                  <input
+                    onBlur={() => checkAmountInputBlur()}
+                    type="text"
+                    value={
+                      +amount > inventoryQuantity ? inventoryQuantity : amount
+                    }
+                    inputMode="numeric"
+                    onChange={(e) => checkAmountInput(e)}
+                    className="focus:outline-none w-[50px] text-center text-lg"
+                  />
+                  <button onClick={() => incrementAmount()} className="p-[6px]">
+                    <Plus
+                      className={cn(
+                        "w-5 h-5",
+                        +amount < inventoryQuantity
+                          ? "text-gray-800 cursor-pointer"
+                          : "text-gray-400 cursor-not-allowed"
+                      )}
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="">
+            <button
+              onClick={() => addToCart()}
+              disabled={isDisableAdd ? true : false}
+              className={cn(
+                "block w-full py-2 text-base md:text-md rounded text-white font-medium transition-all",
+                isDisableAdd
+                  ? "text-zinc-300 bg-zinc-200 cursor-not-allowed"
+                  : "text-white bg-zinc-800 hover:bg-zinc-700"
+              )}
+            >
+              Thêm vào giỏ hàng
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddToCartModal;
--- src/components/main/pageComponents/productDetailPage/detail/Detail.tsx
+++ src/components/main/pageComponents/productDetailPage/detail/Detail.tsx
@@ -2,9 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "@/redux/store";
-import { addProductToCart } from "@/redux/reducer/cart.reducer";
-import { commonSuccessToast } from "@/utils/notify";
-import { IProduct, Variant } from "@/interfaces/product";
+import { IProduct } from "@/interfaces/product";
 import { getProductState } from "@/redux/reducer/product.reducer";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
@@ -15,6 +13,9 @@ import { useCurrency } from "@/hooks";
 import "swiper/css";
 import "swiper/css/pagination";
 import "./detailCustomSwiper.scss";
+import AddToCardModal from "../addToCartModal/AddToCartModal";
+import Link from "next/link";
+import { Facebook, Instagram, Twitter } from "lucide-react";
 
 const ImageItem = ({ src }: { src: string }) => {
   return (
@@ -32,15 +33,10 @@ const ImageItem = ({ src }: { src: string }) => {
 
 const Detail = () => {
   const { productId } = useParams();
-  const user = JSON.parse(localStorage.getItem("user") as string);
   // state
+  const [isOpenModal, setIsOpenModal] = useState(false);
   const productState = useAppSelector((state) => state.product.product);
   const [product, setProduct] = useState<IProduct>({} as IProduct);
-  const [sizeSelected, setSizeSelected] = useState("");
-  const [colorSelected, setColorSelected] = useState("");
-  const [inventoryQuantity, setInventoryQuantity] = useState(0);
-  const [imageSelected, setImageSelected] = useState("");
-  const [quantitySelected, setQuantitySelected] = useState(1);
 
   // useEffect
   const dispatchThunk = useAppDispatch();
@@ -51,74 +47,13 @@ const Detail = () => {
 
   useEffect(() => {
     setProduct(productState);
-    setColorSelected(productState.properties?.[0]?.color);
-    setSizeSelected(productState.properties?.[0]?.variants?.[0].size);
-    setImageSelected(productState.properties?.[0]?.imageUrl);
   }, [productState]);
 
-  useEffect(() => {
-    getProductQuantityExist();
-  }, [colorSelected, sizeSelected]);
-
-  // func
-  const getProductQuantityExist = () => {
-    const indexOfColor = product.properties?.findIndex(
-      (item) => item.color === colorSelected
-    );
-    const indexOfSize = product.properties?.[indexOfColor]?.variants.findIndex(
-      (item) => item.size === sizeSelected
-    );
-    const quantity =
-      product.properties?.[indexOfColor]?.variants?.[indexOfSize]?.quantity;
-    setInventoryQuantity(quantity);
-  };
-
-  const handleChangeImageSelected = (color: string) => {
-    const indexOfColor = product.properties?.findIndex(
-      (item) => item.color === colorSelected
-    );
-    const image = product.properties?.[indexOfColor]?.imageUrl;
-    setImageSelected(image);
-  };
-
-  const convertVariantArr = (arr: Array<Array<Variant>>) => {
-    if (!arr) {
-      return;
-    }
-    const result = arr.map((item) => {
-      return item[0];
-    });
-
-    return result;
-  };
-
-  const addToCart = () => {
-    const userId = user._id;
-
-    const data = {
-      user_id: userId,
-      product: {
-        _id: product._id,
-        name: product.productName,
-        quantity: quantitySelected,
-        imageUrl: imageSelected,
-        color: colorSelected,
-        size: sizeSelected,
-        price: product.price,
-      },
-    };
-
-    if (inventoryQuantity > 0 && inventoryQuantity >= data.product.quantity) {
-      dispatchThunk(addProductToCart(data));
-      commonSuccessToast("Thêm sản phẩm thành cồng");
-    }
-  };
-
-  // variable
   const imageArr = product.properties?.map((item) => item?.imageUrl);
-  const variantArr = convertVariantArr(
-    product.properties?.map((item) => item.variants)
-  );
+
+  const handleCloseModal = () => {
+    setIsOpenModal(false);
+  };
 
   return (
     <>
@@ -137,72 +72,93 @@ const Detail = () => {
           </Swiper>
         </div>
         <div className="flex-[1]">
-          <h3 className="text-lg md:text-2xl font-medium text-gray-800">
+          <h3 className="text-xl md:text-3xl font-medium text-gray-800 pb-2">
             {product.productName}
           </h3>
-          <h4 className="py-1 md:py-3 text-md md:text-xl font-semibold text-gray-800">
+          <h4 className="py-1 md:py-3 text-xl md:text-2xl font-semibold text-[--secondary-color]">
             {useCurrency(product.price)}
           </h4>
 
-          <div className="py-2">
-            <div className="h-[4px] w-[40px] bg-[--secondary-color] rounded"></div>
+
+
+          <div className="hidden md:flex flex-col gap-2 pt-6 pb-3 md:pt-10">
+            <button
+              onClick={() => setIsOpenModal(true)}
+              className="flex-[1] py-3 px-2 w-[300px] rounded bg-zinc-800 text-white text-base md:text-md font-medium hover:bg-zinc-700 transition-all"
+            >
+              Thêm vào giỏ hàng
+            </button>
+            <button className="flex-[1] py-2 w-[300px] px-2 rounded border-2 border-zinc-800 text-gray-800 text-base md:text-md hover:text-gray-700 hover:border-zinc-600 transition-all">
+              Thêm vào yêu thích
+            </button>
           </div>
 
-          <div className="py-4 md:py-6">
-            <div className="flex flex-col sm:flex-row items-center sm:gap-2 w-full">
-              <div className="flex flex-[1] my-1 items-center gap-2 border border-gray-300 rounded px-3 py-2 md:px-4 w-full">
-                <h5 className="text-gray-800 font-medium text-base md:text-md">
-                  Màu:
-                </h5>
-                <select
-                  onChange={(e) => {
-                    setColorSelected(e.target.value);
-                    handleChangeImageSelected(e.target.value);
-                  }}
-                  value={colorSelected}
-                  className="selectTagOfProductDetail outline-none flex-[1]"
-                  name="color"
-                >
-                  {product.properties?.map((item) => (
-                    <option
-                      className="text-gray-800 font-medium text-base md:text-md"
-                      key={uuidv4()}
-                      value={item.color}
-                    >
-                      {item.color}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div className="flex flex-[1] my-1  w-full items-center gap-2 border border-gray-300 rounded px-3 py-2 md:px-4">
-                <h5 className="text-gray-800 font-medium text-base md:text-md">
-                  Size:
-                </h5>
-                <select
-                  onChange={(e) => {
-                    setSizeSelected(e.target.value);
-                  }}
-                  value={sizeSelected}
-                  className="selectTagOfProductDetail outline-none flex-[1]"
-                  name="size"
-                >
-                  {variantArr?.map((item) => (
-                    <option
-                      className="text-gray-800 font-medium text-base md:text-md"
-                      key={uuidv4()}
-                      value={item.size}
-                    >
-                      {item.size}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            </div>
+          <div className="pt-6 pb-3 md:pt-10">
+            <h4 className="text-md md:text-lg text-gray-800 font-semibold pb-2">
+              Mô tả:
+            </h4>
+            <p className="pl-3 md:pl-4 text-base md:text-md font-normal text-gray-600">
+              {product.description}
+            </p>
+          </div>
 
-            <div>{inventoryQuantity || 0}</div>
+          <div className="pt-6 pb-3 md:pt-10">
+            <h4 className="pb-2 text-md md:text-lg text-gray-800 font-semibold">
+              Chia sẻ:
+            </h4>
+            <ul className="pl-3 md:pl-4 text-base md:text-md font-normal text-gray-600 flex items-center gap-3">
+              <li className=" w-[30px] h-[30px] rounded-full border border-zinc-800">
+                <Link
+                  href=""
+                  className="flex items-center justify-center w-full h-full"
+                >
+                  <Facebook className="w-5 h-5  text-gray-800" />
+                </Link>
+              </li>
+              <li className=" w-[30px] h-[30px] rounded-full border border-zinc-800">
+                <Link
+                  href=""
+                  className="flex items-center justify-center w-full h-full"
+                >
+                  <Twitter className="w-5 h-5  text-gray-800" />
+                </Link>
+              </li>
+              <li className=" w-[30px] h-[30px] rounded-full border border-zinc-800">
+                <Link
+                  href=""
+                  className="flex items-center justify-center w-full h-full"
+                >
+                  <Instagram className="w-5 h-5  text-gray-800" />
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
+
+      <div
+        style={{ boxShadow: "0px 0px 6px 0px rgba(0,0,0,0.08)" }}
+        className="block md:hidden fixed z-20 bottom-0 left-0 rounded-t-xl right-0 bg-white px-3 py-5"
+      >
+        <div className="flex flex-row gap-2 w-full">
+          <button
+            onClick={() => setIsOpenModal(true)}
+            className="flex-[1] py-3 px-2 rounded bg-zinc-800 text-white text-base md:text-md font-medium hover:bg-zinc-700 transition-all"
+          >
+            Thêm vào giỏ hàng
+          </button>
+          <button className="flex-[1] py-2 md:py-3 px-2 rounded border-2 border-zinc-800 text-gray-800 text-base md:text-md hover:text-gray-700 hover:border-zinc-600 transition-all">
+            Thêm vào yêu thích
+          </button>
+        </div>
+      </div>
+
+      {/* modal */}
+      <AddToCardModal
+        isOpen={isOpenModal}
+        onClose={handleCloseModal}
+        product={product}
+      />
     </>
   );
 };
