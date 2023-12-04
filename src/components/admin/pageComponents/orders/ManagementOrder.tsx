755f5815501d72ef68f1d3fe0329184f3bb36cfa
QuangSon0409 <buiquangson7782@gmail.com>
2023-12-05 00:38:20 +0700

    update export order

--- src/components/admin/pageComponents/orders/ManagementOrder.tsx
+++ src/components/admin/pageComponents/orders/ManagementOrder.tsx
@@ -3,48 +3,63 @@ import React, { useEffect, useRef, useState } from 'react'
 
 import SearchOrder from './SearchOrder'
 import { IBill } from '@/interfaces/bill';
-import { getAllOrderBill } from '@/services/order/order';
+import { exportBillById, getAllOrderBill } from '@/services/order/order';
 import Toaster from '@/components/Toaster/Toaster';
 import { getAllUser } from '@/services/user/user';
 import { IUser } from '@/interfaces/user';
+import Image from 'next/image';
+import { useCurrency } from '@/hooks';
 
 
 const ManagementOrder = () => {
     const [billAll, setBillAll] = useState<IBill[]>([]);
-    const [bill, setBill] = useState<IBill[]>([]);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [limit, setLimit] = useState<number>(2);
+    const [totalItems, setTotalItems] = useState<number>(1);
+    const [totalPages, setTotalPages] = useState<number>(1);
+
+
+    const [bill, setBill] = useState<IBill[]>([])
     const [user, setUser] = useState<IUser[]>([])
     const toasterRef = useRef<any>();
     useEffect(() => {
-        fetchDataAll(0, Number.MAX_SAFE_INTEGER);
-        fetchAllUser(0, Number.MAX_SAFE_INTEGER)
-    }, [])
-    const fetchDataAll = async (currentpage: number, limit: number) => {
-        const response = await getAllOrderBill(currentpage, limit);
-        if (response) {
-            const data: any = response.data;
-            setBillAll(data)
-        } else {
-            toasterRef.current.showToast("error", "Delete Fail!");
-        }
-    };
-    const fetchAllUser = async (currentPage: number, limit: number) => {
-        const response = await getAllUser(currentPage, limit);
-        if (response) {
-            const data: any = response;
-            setUser(data.data)
-        } else {
+        Promise.all([
+            getAllOrderBill(0, Number.MAX_SAFE_INTEGER),
+            getAllUser(0, Number.MAX_SAFE_INTEGER)
+        ]).then(([orderData, userData]: any) => {
+            setTotalPages(orderData.paginate.totalPages)
+            setTotalItems(orderData.paginate.totalItems)
+            const userMap = new Map(userData?.data?.map((user: any) => [user._id, user.name]));
+            setUser(userData?.data);
+            setBillAll(orderData?.data?.map((item: any) => {
+                return {
+                    ...item,
+                    userName: userMap.get(item.user_id),
+                };
+            }));
+        }).catch((error) => {
+            // Handle errors
+            console.error("Error fetching data: ", error);
+        });
+    }, []);
 
-        }
+
+
+
+
+    console.log("bill", billAll)
+    const customDay = (createdAt: string) => {
+        const date = new Date(createdAt);
+        const formatDate = `${date.getHours()}:${date.getMinutes()}  ${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+        return formatDate
     }
-    billAll.map((item: any) => {
-
-        const userName = user?.filter((user: IUser) => user._id === item.user_id);
-        console.log("userName: ", user)
-        return {
-            ...item,
-            // user_id: userName?[0]?.name
-        }
-    })
+    const exportBill = (id: any) => {
+        // window.location("http")
+        exportBillById(id)
+        window.open(`
+http://localhost:8088/order/export/${id}`)
+        // window.location.reload()
+    }
 
 
 
@@ -71,7 +86,7 @@ const ManagementOrder = () => {
                             </th>
 
                             <th scope="col" className="px-6 py-3">
-                                Id Order
+                                Id
                             </th>
 
                             <th scope="col" className="px-6 py-3">
@@ -87,7 +102,7 @@ const ManagementOrder = () => {
                             </th>
 
                             <th scope="col" className="px-6 py-3">
-                                Status Order
+                                Status
                             </th>
 
                             <th scope="col" className="px-6 py-3">
@@ -101,7 +116,7 @@ const ManagementOrder = () => {
                     </thead>
                     <tbody>
 
-                        {/* {categories.length !== 0 ? categories.map((cate, index) => {
+                        {billAll?.length !== 0 ? billAll?.map((bill, index) => {
                             return (
                                 <tr
                                     key={index}
@@ -113,15 +128,33 @@ const ManagementOrder = () => {
                                     >
                                         {index + 1}
                                     </th>
-                                    <td className="px-6 py-4">{cate.name}</td>
+                                    <td className="px-6 py-4">{bill._id}</td>
+                                    <td className="px-6 py-4">{bill.userName}</td>
+                                    <td className="px-6 py-4 flex justify-between w-[200px]">{bill.items.map((item) => {
+                                        return <>
+                                            <figure className="w-[80px] h-[80px] relative rounded overflow-hidden">
+                                                <Image
+                                                    src={item.property.imageUrl}
+                                                    fill
+                                                    objectFit="contain"
+                                                    className="absolute"
+                                                    alt=""
+                                                />
+                                            </figure>
+                                        </>
+                                    })}</td>
+                                    <td className="px-6 py-4">{useCurrency(bill.total_price)}</td>
+                                    <td className="px-6 py-4">{bill.history_order_status[0].status}</td>
+                                    <td className="px-6 py-4">{customDay(bill.createdAt.toString())}</td>
+                                    <td className="px-6 py-4">
+
+                                    </td>
+
                                     <td className="px-6 py-4 ">
-                                        <div className="flex items-center space-x-4">
+                                        <div className="flex justify-center">
                                             <button
                                                 type="button"
-                                                onClick={() => {
-                                                    setModal(true);
-                                                    setCategory(cate);
-                                                }}
+                                                onClick={() => exportBill(bill._id)}
                                                 data-drawer-show="drawer-update-product"
                                                 aria-controls="drawer-update-product"
                                                 className="py-2 px-3 flex items-center text-sm font-medium text-center bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
@@ -140,46 +173,25 @@ const ManagementOrder = () => {
                                                         clipRule="evenodd"
                                                     />
                                                 </svg>
-                                                Edit
+                                                Export
                                             </button>
 
-                                            <button
-                                                type="button"
-                                                onClick={() => { handleDelete(cate._id), setCategory(cate); }}
-                                                data-modal-target="delete-modal"
-                                                data-modal-toggle="delete-modal"
-                                                className="flex items-center text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
-                                            >
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    className="h-4 w-4 mr-2 -ml-0.5"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                    aria-hidden="true"
-                                                >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                                        clipRule="evenodd"
-                                                    />
-                                                </svg>
-                                                Delete
-                                            </button>
                                         </div>
                                     </td>
                                 </tr>
                             );
-                        }) : ( */}
-                        <tr>
-                            <td colSpan={3} className=" p-4  text-center bg-white  ">
-                                <div className="flex align-center justify-center ">
-                                    Không có dữ liệu <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="w-6 h-5 pl-2" width="100" height="100" viewBox="0 0 48 48">
-                                        <path d="M 8.5 8 C 6.019 8 4 10.019 4 12.5 L 4 18 L 16.052734 18 C 16.636734 18 17.202344 17.793922 17.652344 17.419922 L 23.5 12.546875 L 19.572266 9.2734375 C 18.586266 8.4524375 17.336734 8 16.052734 8 L 8.5 8 z M 27.644531 13 L 19.572266 19.724609 C 18.585266 20.546609 17.336734 21 16.052734 21 L 4 21 L 4 35.5 C 4 37.981 6.019 40 8.5 40 L 39.5 40 C 41.981 40 44 37.981 44 35.5 L 44 17.5 C 44 15.019 41.981 13 39.5 13 L 27.644531 13 z"></path>
-                                    </svg>
-                                </div>
-                            </td>
-                        </tr>
-                        {/* )} */}
+                        }) : (
+                            <tr>
+                                <td colSpan={3} className=" p-4  text-center bg-white  ">
+                                    <div className="flex align-center justify-center ">
+                                        Không có dữ liệu <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="w-6 h-5 pl-2" width="100" height="100" viewBox="0 0 48 48">
+                                            <path d="M 8.5 8 C 6.019 8 4 10.019 4 12.5 L 4 18 L 16.052734 18 C 16.636734 18 17.202344 17.793922 17.652344 17.419922 L 23.5 12.546875 L 19.572266 9.2734375 C 18.586266 8.4524375 17.336734 8 16.052734 8 L 8.5 8 z M 27.644531 13 L 19.572266 19.724609 C 18.585266 20.546609 17.336734 21 16.052734 21 L 4 21 L 4 35.5 C 4 37.981 6.019 40 8.5 40 L 39.5 40 C 41.981 40 44 37.981 44 35.5 L 44 17.5 C 44 15.019 41.981 13 39.5 13 L 27.644531 13 z"></path>
+                                        </svg>
+                                    </div>
+                                </td>
+                            </tr>
+                        )}
+
                     </tbody>
                 </table>
             </div>
