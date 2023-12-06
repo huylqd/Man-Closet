ffd3782787a0d0fbff85f4885e8b3c6125e7d2b6
huylqd <huylq.12bhla@gmail.com>
2023-12-06 23:57:40 +0700

    use refreshToken to call acceccToken

--- src/services/instance.ts
+++ src/services/instance.ts
@@ -1,21 +1,56 @@
 import axios from "axios";
 
+interface MyResponseData {
+  accessToken: string; // or the actual type of your accessToken
+  // other properties if there are any
+}
 const instance = axios.create({
     baseURL: "http://localhost:8088/",
     headers: {
-      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      // Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      "Content-Type": "application/json",
     },
 });
 
-instance.interceptors.response.use(
-  function (response) {
-    return response.data;
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config
   },
-  function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error);
+  (error) => {
+    return Promise.reject(error)
   }
 );
+
+instance.interceptors.response.use(
+  (res) => {
+    return res.data
+  },
+  async (err) => {
+    const originalConfig = err.config;
+
+    if(originalConfig.url !== '/signIn' && err.response) {
+      if(err.response.status === 401 && !originalConfig._retry) {
+        originalConfig._retry = true
+      }
+      try {
+
+        const rs = await instance.post('refreshToken', {
+          refreshToken: localStorage.getItem("refresh")
+        })
+
+        const accessToken  = rs.data
+        localStorage.setItem("accessToken", accessToken);
+        return instance(originalConfig)
+      } catch (error) {
+        return Promise.reject(error)
+      }
+    }
+  }
+)
 
 export default instance;
