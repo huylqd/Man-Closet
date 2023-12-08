ec2539b369ceca185f66480b1b46150a36b39b99
QuangSon0409 <buiquangson7782@gmail.com>
2023-12-09 03:37:25 +0700

    update bill status

--- src/redux/reducer/order.reducer.ts
+++ src/redux/reducer/order.reducer.ts
@@ -1,22 +1,28 @@
 
+import { IBill } from "@/interfaces/bill"
 import { OrderItem } from "@/interfaces/order.interface"
 import { ProductSold } from "@/interfaces/product"
-import { getAllOrderBill, getProductSold } from "@/services/order/order"
+import { getAllOrderBill, getProductSold, updateBill } from "@/services/order/order"
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
 interface OrderState {
   productSold: ProductSold[]
-  orders: OrderItem[]
+  orders: OrderItem[],
+  updateBill: IBill[]
 }
-
-const initialState : OrderState = {
+interface IUpdateProps {
+  billId: string;
+  status: string
+}
+const initialState: OrderState = {
   productSold: [],
-  orders: []
+  orders: [],
+  updateBill: []
 }
 
 export const getProductSoldState = createAsyncThunk(
   "order/getProductSold",
-  async(_, thunkAPI) => {
+  async (_, thunkAPI) => {
     const response = await getProductSold()
     return response.data
   }
@@ -24,25 +30,37 @@ export const getProductSoldState = createAsyncThunk(
 
 export const getAllOrderBillState = createAsyncThunk(
   "order/getAllOrderBill",
-  async(_, thunkAPI) => {
+  async (_, thunkAPI) => {
     const response = await getAllOrderBill(0, Number.MAX_SAFE_INTEGER)
     return response.data
   }
 )
+export const changeStatusBillState = createAsyncThunk(
+  "order/updateStatusBillState",
+  async (data: IUpdateProps) => {
+    const { billId, status } = data
+    const response = await updateBill(billId, status)
+    return response.data
+  }
+)
+
 
 const orderSlice = createSlice({
   name: "order",
   initialState,
   reducers: {},
-  extraReducers(builder){
+  extraReducers(builder) {
     builder.addCase(getProductSoldState.fulfilled, (state, action) => {
       state.productSold = action.payload
     }),
-    builder.addCase(getAllOrderBillState.fulfilled, (state, action) => {
-      state.orders = action.payload
+      builder.addCase(getAllOrderBillState.fulfilled, (state, action) => {
+        state.orders = action.payload
+      })
+    builder.addCase(changeStatusBillState.fulfilled, (state, action) => {
+      state.updateBill = action.payload
     })
   }
 })
 
-export const {} = orderSlice.actions
+export const { } = orderSlice.actions
 export default orderSlice.reducer
