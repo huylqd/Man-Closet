291f4feda3c4e29493b2488ff6bbcdfcb89de654
duc1105 <trananhduc628@gmail.com>
2023-12-19 23:40:03 +0700

    contact

--- src/components/main/pageComponents/home/contact/PageContact.tsx
+++ src/components/main/pageComponents/home/contact/PageContact.tsx
@@ -10,48 +10,18 @@ const PageContact = () => {
     <section>
         <Breadcrumb />
       </section>
-      <div className="container my-5 mx-auto md:px-6">
+      <div className="section_container my-5 mx-auto md:px-6">
   <section className="mb-6">
-    <div className="relative rounded h-[300px] overflow-hidden bg-cover bg-[50%] bg-no-repeat bg-[url('https://file.hstatic.net/200000321971/article/banner-thoi-trang-nam_67798f164e8a4369b257a4013808ff34_1024x1024.jpg')]"></div>
+    <div className="hidden md:block relative rounded h-[300px] overflow-hidden bg-cover bg-[50%] bg-no-repeat bg-[url('https://file.hstatic.net/200000321971/article/banner-thoi-trang-nam_67798f164e8a4369b257a4013808ff34_1024x1024.jpg')]"></div>
     <div className="container px-6 md:px-12">
       <div
         className="block rounded-lg bg-[hsla(0,0%,100%,0.8)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-[hsla(0,0%,5%,0.7)] dark:shadow-black/20 md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px]">
-        <div className="flex flex-wrap">
+        <div className="">
           <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:px-3 lg:mb-0 lg:w-5/12 lg:px-6">
-            <form>
-              <div className="relative mb-6" data-te-input-wrapper-init>
-                <input type="text"
-                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleInput90" placeholder="Name" />
-                <label
-                  className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                  htmlFor="exampleInput90">Name
-                </label>
-              </div>
-              <div className="relative mb-6" data-te-input-wrapper-init>
-                <input type="email"
-                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleInput91" placeholder="Email address" />
-                <label
-                  className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                  htmlFor="exampleInput91">Email address
-                </label>
-              </div>
-              <div className="relative mb-6" data-te-input-wrapper-init>
-                <textarea
-                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleFormControlTextarea1" rows={3} placeholder="Your message"></textarea>
-                <label htmlFor="exampleFormControlTextarea1"
-                  className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Message</label>
-              </div>
-              <button type="button" data-te-ripple-init data-te-ripple-color="light"
-                className="mb-6 inline-block w-full rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] lg:mb-0">
-                Send
-              </button>
-            </form>
+           
           </div>
           <div className="w-full shrink-0 grow-0 basis-auto lg:w-7/12">
-            <div className="flex flex-wrap">
+            <div className="md:flex ">
             <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:w-full lg:px-6 xl:w-6/12">
                 <div className="flex items-start">
                   <div className="shrink-0">
@@ -74,7 +44,7 @@ const PageContact = () => {
                   </div>
                 </div>
               </div>
-              <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:w-full lg:px-6 xl:w-6/12">
+              <div className="mb-12 ml-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:w-full lg:px-6 xl:w-6/12">
                 <div className="flex items-start">
                   <div className="shrink-0">
                   <div className="inline-block rounded-md bg-primary-100 p-4 text-primary">
@@ -90,7 +60,7 @@ const PageContact = () => {
                     <p className="mb-2 font-bold dark:text-white">
                       Trụ Sở Chính
                     </p>
-                    <p className="text-neutral-500 text-sm  dark:text-neutral-200">
+                    <p className="text-neutral-500  dark:text-neutral-200">
                     Phố Trịnh Văn Bô , Phường Phương Canh , quận Từ Liêm
                     </p>
                   </div>
@@ -109,7 +79,7 @@ const PageContact = () => {
                   <div className="ml-6 grow">
                     <p className="mb-2 font-bold dark:text-white">Số điện thoại</p>
                     <p className="text-neutral-500 dark:text-neutral-200">
-                      0327849503
+                      0325587305
                     </p>
                     
                   </div>
@@ -123,11 +93,15 @@ const PageContact = () => {
     </div>
   </section>
 
-
-<div id="map" className= "rounded relative h-[300px] overflow-hidden bg-cover bg-[50%] bg-no-repeat">
+<div className='mb-5 font-bold flex '>
+  <h3>Địa chỉ cửa hàng</h3>
+  <hr  className='w-200px h-10' />
+</div>
+<div id="map" className= "rounded relative h-[400px] overflow-hidden bg-cover bg-[50%] bg-no-repeat">
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11672.945750644447!2d-122.42107853750231!3d37.7730507907087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858070cc2fbd55%3A0xa71491d736f62d5c!2sGolden%20Gate%20Bridge!5e0!3m2!1sen!2sus!4v1619524992238!5m2!1sen!2sus"
-      width="100%" height="480" style={{border: 0}} allowFullScreen loading="lazy"></iframe>
+    className='rounded'
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.8353521255826!2d105.72923707433914!3d21.03927298741918!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31345550b525aa03%3A0x3fdefc40f69a023a!2zQ2FvIMSR4bqzbmcgRlBUIFBo4buRIFRy4buLbmggVsSDbiBCw7QgLCBQaMaw4budbmcgUGjGsMahbmcgQ2FuaCAsIHF14bqtbiBU4burIExpw6pt!5e0!3m2!1svi!2s!4v1703000867309!5m2!1svi!2s" 
+      width="100%" height="650" style={{border: 0}} allowFullScreen loading="lazy"></iframe>
   </div>
 
 </div>
